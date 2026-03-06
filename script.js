1fac52ba1caf06ab111c6b1d98a0d1e1bc3e8a5b
putra-sys <159219055+putra-sys@users.noreply.github.com>
2026-03-06 16:38:19 +0700

    Refactor fetch call and update API URL
    Updated API URL and refactored the fetch call to include user media and geolocation data.

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyd2H5EOp6_482xWjuMhyjMItiL4pmoPpS_rll2eoRAk_b8uziYYZ9VyPU0dQCd0Pcb/exec";
+const API_URL="https://script.google.com/macros/s/AKfycbwtc0exPXKiDr73hpEEX4RRcfYc0raPsNiyW_EBs_2LEjZXYvJC-LywKYlkfRKiHFPC/exec"
 
-fetch(API_URL, {
-  method: "POST",
-  body: JSON.stringify({
-    nama: nama,
-    jam: new Date().toLocaleTimeString(),
-    gps: lokasi,
-    foto: foto
-  })
+let video=document.getElementById("video")
+
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream=>{
+video.srcObject=stream
 })
-.then(res => res.text())
-.then(data => alert("Absensi berhasil"));
+
+function ambilFoto(){
+
+let canvas=document.createElement("canvas")
+
+canvas.width=video.videoWidth
+canvas.height=video.videoHeight
+
+let ctx=canvas.getContext("2d")
+
+ctx.drawImage(video,0,0)
+
+window.foto=canvas.toDataURL()
+
+}
+
+function absen(){
+
+let nama=document.getElementById("nama").value
+let jam=new Date().toLocaleString()
+
+navigator.geolocation.getCurrentPosition(function(pos){
+
+let lokasi=pos.coords.latitude+","+pos.coords.longitude
+
+fetch(API_URL,{
+
+method:"POST",
+
+body:JSON.stringify({
+
+nama:nama,
+jam:jam,
+lokasi:lokasi,
+foto:window.foto
+
+})
+
+})
+
+document.getElementById("status").innerHTML="Absensi berhasil"
+
+})
+
+}
